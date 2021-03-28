1821d097939e3bc5f72110ac8053d392d3644339
dragon3hgmx <dragon3h@gmx.com>
2021-03-28 09:35:23 -0400

    added kubernetese config files for auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,13 @@
 import express from 'express';
-import {json} from 'body-parser'
+import { json } from 'body-parser'
 
 const app = express();
 app.use(json());
 
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('hi Romr!')
+})
+
 app.listen(3000, () => {
-  console.log(`Listening on port 3000`);
+  console.log(`Listening on port 3000!!`);
 })
